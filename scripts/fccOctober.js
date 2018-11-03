2e57ddf4b0c7e6d642e4ae64cd0574d67d000058
Michael Chrisco <michaelachrisco@gmail.com>
2018-11-03 14:15:43 -0700

    Remove hard set calendar events in favor of meetup ical api

--- scripts/fccOctober.js
+++
@@ -1,7 +0,0 @@
-var cal = ics();
-
-function downloadIcs() {
-  cal_single = ics();
-  cal_single.addEvent('freeCodeCamp Fresno meetup', 'Come code with us at Root Access Hackerspace', 'Fresno, CA', '10/06/2018 1:00 pm', '10/06/2018 3:00 pm');
-  cal_single.download();
-}
